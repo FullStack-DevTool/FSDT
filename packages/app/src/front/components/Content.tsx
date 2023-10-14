e9a63b87e9803eea100b9ea99793fb1bfc15e961
Maxime Perraud <contact@maxime-p.dev>
2023-10-15 00:16:37 +0200

    removing fake data

--- packages/app/src/front/components/Content.tsx
+++ packages/app/src/front/components/Content.tsx
@@ -1,25 +1,12 @@
 import styled from "@emotion/styled";
-import ListRenderer from "./ListRenderer";
-import {useSearchStore} from "../stores/searchStore";
+import { useSearchStore } from "../stores/searchStore";
 
 const StyledContent = styled.main`
   padding: 8px;
 `;
 
-const fakeData = [
-  {
-    date: "2019-01-01",
-    source: "source",
-    type: "type",
-    content: "content"
-  }
-];
-
 export default function Content() {
   const search = useSearchStore((state) => state.search);
 
-  return <StyledContent>
-    <ListRenderer rowData={fakeData} />
-    {search}
-  </StyledContent>;
+  return <StyledContent>{search}</StyledContent>;
 }
