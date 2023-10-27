53e102c0b23e9bbbc684759a576c5bbf601db71d
Maxime Perraud <contact@maxime-p.dev>
2023-10-27 11:36:48 +0200

    printing logs into ListView

--- packages/app/src/front/components/Content.tsx
+++ packages/app/src/front/components/Content.tsx
@@ -1,30 +1,31 @@
-import styled from "@emotion/styled";
-import { useSearchStore } from "../stores/searchStore";
-import { useViewFilterStore } from "../stores/viewFilterStore";
-import ListView from "../views/ListView";
-import ColumnView from "../views/ColumnView";
-import AccordionView from "../views/AccordionView";
+import styled from '@emotion/styled'
+import { useSearchStore } from '../stores/searchStore'
+import { useViewFilterStore } from '../stores/viewFilterStore'
+import ListView from '../views/ListView'
+import ColumnView from '../views/ColumnView'
+import AccordionView from '../views/AccordionView'
 
 const StyledContent = styled.main`
+  flex: 1;
   padding: 8px;
-`;
+`
 
 export default function Content() {
-  const search = useSearchStore((state) => state.search);
-  const view = useViewFilterStore((state) => state.view);
-  const source = useViewFilterStore((state) => state.source);
-  const tag = useViewFilterStore((state) => state.tag);
+  const search = useSearchStore((state) => state.search)
+  const view = useViewFilterStore((state) => state.view)
+  const source = useViewFilterStore((state) => state.source)
+  const tag = useViewFilterStore((state) => state.tag)
 
   const renderView = () => {
     switch (view) {
-      case "List":
-        return <ListView />;
-      case "Column":
-        return <ColumnView />;
-      case "Accordion":
-        return <AccordionView />;
+      case 'List':
+        return <ListView />
+      case 'Column':
+        return <ColumnView />
+      case 'Accordion':
+        return <AccordionView />
     }
-  };
+  }
 
-  return <StyledContent>{renderView()}</StyledContent>;
+  return <StyledContent>{renderView()}</StyledContent>
 }
--- packages/app/src/front/components/ListRenderer.tsx
+++ packages/app/src/front/components/ListRenderer.tsx
@@ -1,24 +1,32 @@
-import { AgGridReact } from "ag-grid-react";
-import { FsdtServerMessage } from "@fullstack-devtool/core";
+import { AgGridReact } from 'ag-grid-react'
+import { FsdtServerMessage } from '@fullstack-devtool/core'
 
-import "ag-grid-community/styles/ag-grid.css";
-import { useState } from "react";
-import { ColDef } from "ag-grid-community";
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-material.min.css'
+import { ColDef } from 'ag-grid-community'
+import styled from '@emotion/styled'
 
 interface IProps {
-  rowData: FsdtServerMessage[];
+  rowData: FsdtServerMessage[]
 }
 
-const cols: ColDef<FsdtServerMessage> = [
-  { headerName: "Source", field: "source" },
-  { headerName: "Content", field: "content" },
-  { headerName: "Time", field: "timestamp" },
-  { headerName: "Level", field: "level" },
-  { headerName: "Tag", field: "tag" },
-  { headerName: "Message", field: "message" },
-];
+const cols: ColDef<FsdtServerMessage>[] = [
+  { headerName: 'Source', field: 'source', width: 100 },
+  { headerName: 'Content', field: 'data.content', flex: 1 },
+  { headerName: 'Time', field: 'data.timestamp' },
+  { headerName: 'Level', field: 'data.level', width: 100 },
+  { headerName: 'Tag', field: 'data.tag' },
+]
+
+const StyledListRenderer = styled.div`
+  width: 100%;
+  height: 100%;
+`
 
 export default function ListRenderer({ rowData }: IProps) {
-  const [columnDefs, set] = useState(cols);
-  return <AgGridReact rowData={rowData} columnDefs={columnDefs} />;
+  return (
+    <StyledListRenderer className="ag-theme-material">
+      <AgGridReact rowData={rowData} columnDefs={cols} suppressScrollOnNewData />
+    </StyledListRenderer>
+  )
 }
