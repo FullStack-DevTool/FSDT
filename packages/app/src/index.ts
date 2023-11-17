fce2a10a9277d30918d8fe0b7b0a13c86cc9c5da
Maxime Perraud <contact@maxime-p.dev>
2023-11-17 10:12:22 +0100

    fix: default window size + min window size

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -15,8 +15,10 @@ if (require('electron-squirrel-startup')) {
 const createWindow = async (): Promise<void> => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    height: 600,
-    width: 800,
+    height: 800,
+    width: 1200,
+    minHeight: 600,
+    minWidth: 960,
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
