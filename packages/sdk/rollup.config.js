667c606f17c98ec5d2ea739b0f9da2af6a180f4b
Mathieu <mathieu.raimbault@hotmail.com>
2023-12-02 13:00:33 +0100

    fix(sdk): remove ws from build

--- packages/sdk/rollup.config.js
+++ packages/sdk/rollup.config.js
@@ -1,32 +1,53 @@
 const resolve = require('@rollup/plugin-node-resolve');
-const commonjs = require("@rollup/plugin-commonjs");
-const ts = require("rollup-plugin-ts");
+const commonjs = require('@rollup/plugin-commonjs');
+const ts = require('rollup-plugin-ts');
+// const buildType = process.env.BUILD_TYPE || 'node';
 
 module.exports = [
+  // {
+  //   // UMD
+  //   input: "src/index.ts",
+  //   plugins: [
+  //     commonjs(),
+  //     resolve(),
+  //     babel({
+  //       babelHelpers: "bundled",
+  //     }),
+  //     terser(),
+  //     ts(),
+  //     nodePolyfills()
+  //   ],
+  //   output: {
+  //     file: `dist/index.min.js`,
+  //     format: "umd",
+  //     name: "fullstackDevtoolSdk",
+  //     esModule: false,
+  //     exports: "named",
+  //     sourcemap: true,
+  //   },
+  // },
   {
-   input: 'src/index.ts', // Adjust the entry point based on your project structure
-   output: [
-      // If the app using this library supports esmodules, it will use the esm build, otherwise cjs build will be used.
+    input: 'src/index.ts', // Adjust the entry point based on your project structure
+    output: [
       {
-        file: "dist/index.cjs.js",
-        format: "cjs",
+        file: `dist/index.cjs.js`,
+        format: 'cjs',
         sourcemap: false,
+        inlineDynamicImports: true,
       },
       {
-        file: "dist/index.esm.js",
-        format: "esm",
+        file: `dist/index.esm.js`,
+        format: 'esm',
         sourcemap: false,
+        inlineDynamicImports: true,
       },
     ],
-  plugins: [
-    resolve(), // Use the rollup-plugin-node-resolve to resolve dependencies
-    commonjs(),
-    ts({})
-    // typescript({
-    //   rollupCommonJSResolveHack: false,
-    //   clean: true,
-    // }),
-    // Add other plugins as needed
-  ],
-},
-]
+    external: [/node_modules/],
+    // external: [ /^node:/],
+    plugins: [
+      resolve(), // Use the rollup-plugin-node-resolve to resolve dependencies
+      commonjs(),
+      ts({}),
+    ],
+  },
+];
