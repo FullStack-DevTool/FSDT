417759b49f272f7bae83626001556373bd8d5a3b
Mathieu <mathieu.raimbault@hotmail.com>
2023-12-08 16:56:41 +0100

    fix(app): fonts

--- packages/app/src/front/utils/globalStyle.ts
+++ packages/app/src/front/utils/globalStyle.ts
@@ -1,56 +1,82 @@
-import { Interpolation } from '@emotion/react'
-import { theme, Theme } from '../theme'
+import { css } from '@emotion/react'
+import { theme } from '../theme'
 
-export const globalStyle: Interpolation<Theme> = {
-  body: {
-    margin: 0,
-  },
-  '#app': {
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-  },
+export const globalStyleCss = css`
+  * {
+    font-family: ${theme.fontFamily.primary};
+  }
+  body {
+    margin: 0;
+  }
+  #app {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
   /*
 
   AG Grid
 
   */
-  '.ag-header, .ag-header-row, .ag-header-cell': {
-    minHeight: '0 !important',
-    height: '0 !important',
-    borderBottom: '0 !important',
-  },
+  .ag-header,
+  .ag-header-row,
+  .ag-header-cell {
+    min-height: 0 !important;
+    height: 0 !important;
+    border-bottom: 0 !important;
+  }
 
   /* Row */
 
-  '.ag-row': {
-    borderBottom: 'none',
-  },
+  .ag-row {
+    border-bottom: none;
+  }
 
-  '.ag-row::before,.ag-row-hover::before': {
-    content: '""',
-    position: 'absolute',
-    bottom: 0,
-    left: '5% !important',
-    width: '90%',
-    borderBottom: '1px solid #e8e8e8',
-  },
+  .ag-row::before,
+  .ag-row-hover::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 5% !important;
+    width: 90%;
+    border-bottom: 1px solid #e8e8e8;
+  }
 
-  '.ag-row-selected': {
-    borderTop: `1px solid ${theme.colors.activeBorder}`,
-    borderBottom: `1px solid ${theme.colors.activeBorder}`,
-  },
+  .ag-row-selected {
+    border-top: 1px solid ${theme.colors.activeBorder};
+    border-bottom: 1px solid ${theme.colors.activeBorder};
+  }
 
-  '.ag-row-selected::before,.ag-row-hover.ag-row-selected::before': {
-    backgroundColor: 'transparent',
-    backgroundImage: 'none',
-  },
+  .ag-row-selected::before,
+  .ag-row-hover.ag-row-selected::before {
+    background-color: transparent;
+    background-image: none;
+  }
 
-  /* Cell */
+  .ag-cell {
+    display: flex;
+    align-items: center;
+    line-height: normal;
+  }
 
-  '.ag-cell': {
-    display: 'flex',
-    alignItems: 'center',
-    lineHeight: 'normal',
-  },
-}
+  @font-face {
+    font-family: 'Fira Code';
+    font-style: normal;
+    font-weight: 400;
+    src: url('/fonts/FiraCode-Regular.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 700;
+    src: url('/fonts/LexendDeca-Bold.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Lexend';
+    font-style: normal;
+    font-weight: 400;
+    src: url('/fonts/Lexend-Regular.woff2') format('woff2');
+  }
+`
